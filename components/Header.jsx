b1e01b6144a6820106d71081498d3975b7dbf168
samuelberhane <1samibrhane@gmail.com>
2023-02-20 10:27:05 +0300

    Create Header component.

---
+++ components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between py-4 shadow px-4 md:px-8 lg:px-12">
+      <h1 className="font-[Pacifico] text-lg md:text-xl lg:text-2xl xl:text-4xl">
+        Samuel Brhane
+      </h1>
+      <div className="gap-6 items-center hidden md:flex">
+        <ul className="flex items-center gap-6 lg:gap-8 font-semibold">
+          <li>Home</li>
+          <li>Skills</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+        <button className="bg-green-600 font-bold text-white shadow-4xl  rounded-2xl px-4 py-2 hidden">
+          Night
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
